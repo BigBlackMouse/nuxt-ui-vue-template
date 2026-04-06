8b04a9b08fe9eb965280def99f708f622b7c2259
邓守彬 <3210289702@qq.com>
2026-04-06 16:36:21 +0800

    base修改为./

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import ui from '@nuxt/ui/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [
     vueRouter({
       dts: 'src/route-map.d.ts',
