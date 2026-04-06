17fb14c4902520f33ef8c73496860cfcb987a56d
邓守彬 <3210289702@qq.com>
2026-04-06 16:04:10 +0800

    修改base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import ui from '@nuxt/ui/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nuxt-ui-template-dashboard-vue/',
   plugins: [
     vueRouter({
       dts: 'src/route-map.d.ts'
