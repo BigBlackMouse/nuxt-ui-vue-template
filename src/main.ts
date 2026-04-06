00a52b68bcf08dadd6d286f03d68eae2559f6186
邓守彬 <3210289702@qq.com>
2026-04-06 17:30:38 +0800

    main.ts-history: createWebHashHistory()

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/css/main.css'
 
 import { createApp } from 'vue'
 import type { RouteRecordRaw } from 'vue-router'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createHead } from '@unhead/vue/client'
@@ -15,7 +15,7 @@ const app = createApp(App)
 const head = createHead()
 const router = createRouter({
   routes: setupLayouts(routes as RouteRecordRaw[]),
-  history: createWebHistory()
+  history: createWebHashHistory()
 })
 
 app.use(head)
